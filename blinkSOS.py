c6f36ecda44037885069c52a073b36c87f18987f
Eugen Kolesnik <eugen.kolesnik@gmail.com>
2019-02-23 12:42:03 +0100

    Blink SOS on 7th GPIO

---
+++ blinkSOS.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+dot = 0.2
+dash =  dot*3
+gap = dot*7
+
+def blinkShort ():
+    GPIO.output(7, 1)
+    time.sleep(dot)
+    GPIO.output(7, 0)
+    return;
+
+def blinkLong ():
+    GPIO.output(7, 1)
+    time.sleep(dash)
+    GPIO.output(7, 0)
+    return;
+
+def blinkS ():
+    blinkShort()
+    time.sleep(dot)
+    blinkShort()
+    time.sleep(dot)
+    blinkShort()
+    return;
+
+def blinkO ():
+    blinkLong()
+    time.sleep(dot)
+    blinkLong()
+    time.sleep(dot)
+    blinkLong()
+    return;
+
+def blinkSOS():
+    blinkS()
+    time.sleep(dash)
+    blinkO()
+    time.sleep(dash)
+    blinkS()
+    return;
+
+while True:
+    blinkSOS()
+    time.sleep(gap)
+
